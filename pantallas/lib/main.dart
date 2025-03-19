1bf67899c810e88a6bbc08983d4f372a8a7a5115
Abjuraa <107774277+Abjuraa@users.noreply.github.com>
2025-03-19 09:23:34 -0500

    paginas

---
+++ pantallas/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: body(),
+    );
+  }
+}
+
+class body extends StatefulWidget {
+  const body({super.key});
+
+  @override
+  State<body> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<body> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pantalla 1'), leading: null,
+        automaticallyImplyLeading: false,
+        ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hola Mundo', style: TextStyle(fontSize: 30)),
+            Text('Bienvenidos a Flutter', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Pantalla2()),
+                );
+              },
+              child: Text('Ir a la pantalla 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Pantalla3()),
+                );
+              },
+              child: Text('Ir a la pantalla 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Pantalla2 extends StatelessWidget {
+  const Pantalla2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pantalla 2'), leading: null),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hola Pantalla 2', style: TextStyle(fontSize: 30)),
+            Text('Bienvenidos a la pantalla 2', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(
+                  context,
+                  MaterialPageRoute(builder: (context) => body()),
+                );
+              },
+              child: Text('Ir a la pantalla 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Pantalla3()),
+                );
+              },
+              child: Text('Ir a la pantalla 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Pantalla3 extends StatelessWidget {
+  const Pantalla3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Pantalla 3'), leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () => Navigator.pop(context)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hola Pantalla 3', style: TextStyle(fontSize: 30)),
+            Text('Bienvenidos a la pantalla 3', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => body()),
+                );
+              },
+              child: Text('Ir a la pantalla 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Pantalla2()),
+                );
+              },
+              child: Text('Ir a la pantalla 2'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
